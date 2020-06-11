8d430ee22b77ec156c0ff2a54822cc8f987528a2
Blurgyy <blurgyy@gmail.com>
2020-06-11 22:34:50 +0800

    Format code with yapf
    Signed-off-by: Blurgyy <blurgyy@gmail.com>

--- lec01/shadertoy.py
+++ lec01/shadertoy.py
@@ -12,18 +12,21 @@ w, h = wh = (dim, dim)
 res = ti.Vector([w, h])
 pixels = ti.Vector(n=3, dt=ti.f32, shape=wh)
 
+
 @ti.func
 def setPixel(i: ti.i32, j: ti.i32, iTime: ti.f32):
     coord = ti.Vector([i, j])
     uv = coord / res
-    uvxyx = ti.Vector([uv[0], uv[1]+2, uv[0]+4])
+    uvxyx = ti.Vector([uv[0], uv[1] + 2, uv[0] + 4])
     color = 0.5 + 0.5 * ti.cos(iTime + uvxyx)
     pixels[i, j] = color
+
 
 @ti.kernel
 def paint(iTime: ti.f32):
     for i, j in pixels:
         setPixel(i, j, iTime)
+
 
 def main():
     gui = ti.GUI("test", res=wh)
@@ -31,9 +34,10 @@ def main():
     for t in range(int(1e9)):
         if gui.get_event(ti.GUI.ESCAPE):
             sys.exit()
-        paint(t/1e2)
+        paint(t / 1e2)
         gui.set_image(pixels.to_numpy())
         gui.show()
+
 
 if __name__ == "__main__":
     main()
